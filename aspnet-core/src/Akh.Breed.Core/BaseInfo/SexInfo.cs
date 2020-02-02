26454162ff7b69347e4f665ca081902ed756ffaa
Amir akhavan <a.akhavan@centralmun.imcso.net>
2020-02-02 11:28:51 +0330

    add baseinfo

---
+++ aspnet-core/src/Akh.Breed.Core/BaseInfo/SexInfo.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations.Schema;
+using Abp.Domain.Entities;
+using Abp.Domain.Entities.Auditing;
+using Abp.Timing;
+
+namespace Akh.Breed.BaseInfo
+{
+
+    [Table("AkhSexInfo")]
+    public class SexInfo : Entity, IHasCreationTime, IMayHaveTenant
+    {
+        public string Code { get; set; }
+
+        public string Name { get; set; }
+
+        public DateTime CreationTime { get; set; }
+
+        public int? TenantId { get; set; }
+
+        protected SexInfo()
+        {
+            CreationTime = Clock.Now;
+        }
+    }
+}
