da489c00d93141fc8b37dcf246ab3738fb2e1f96
kharazian <amirkharazian@gmail.com>
2020-02-05 02:34:19 +0330

    first publish

--- angular/src/app/shared/layout/themes/default/default-brand.component.ts
+++ angular/src/app/shared/layout/themes/default/default-brand.component.ts
@@ -10,7 +10,7 @@ import { DOCUMENT } from '@angular/common';
 })
 export class DefaultBrandComponent extends AppComponentBase {
 
-    defaultLogo = AppConsts.appBaseUrl + '/assets/common/images/app-logo-on-' + this.currentTheme.baseSettings.menu.asideSkin + '.svg';
+    defaultLogo = AppConsts.appBaseUrl + '/assets/common/images/logo1.png';//app-logo-on-' + this.currentTheme.baseSettings.menu.asideSkin + '.svg';
     remoteServiceBaseUrl: string = AppConsts.remoteServiceBaseUrl;
 
     constructor(
