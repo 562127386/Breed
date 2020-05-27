a7547bb864855c1a29d824e1f5f7814736c1cc44
kharazian <amirkharazian@gmail.com>
2020-05-27 23:27:51 +0430

    report run

--- angular/src/app/main/herdCertificate/herdCertificate.component.ts
+++ angular/src/app/main/herdCertificate/herdCertificate.component.ts
@@ -18,6 +18,7 @@ export class HerdCertificateComponent extends AppComponentBase implements OnInit
     designer: any;
     viewer: any;
     report: any;
+    dataSet: any;
 
     constructor(
         injector: Injector
@@ -28,34 +29,20 @@ Stimulsoft.Base.StiLicense.Key = "6vJhGtLLLz2GNviWmUTrhSqnOItdDwjBylQzQcAOiHl2AD
     }
   
     ngOnInit() {
-        // console.log('Loading Designer view');
-    
-        // console.log('Set full screen mode for the designer');
-        // this.options = new Stimulsoft.Designer.StiDesignerOptions();
-        // this.options.appearance.fullScreenMode = false;
-    
-        // console.log('Create the report designer with specified options');
-        // this.designer = new Stimulsoft.Designer.StiDesigner(this.options, 'StiDesigner', false);
-    
-        // console.log('Edit report template in the designer');
-        // this.designer.report = new Stimulsoft.Report.StiReport();
-    
-        // console.log('Load report from url');
-        // this.designer.report.loadFile('/assets/reports/SimpleList.mrt');
-    
-        // console.log('Rendering the designer to selected element');
-        // this.designer.renderHtml('designer');
-    
-        // console.log('Loading completed successfully!');
-Stimulsoft.Base.StiLicense.Key = "6vJhGtLLLz2GNviWmUTrhSqnOItdDwjBylQzQcAOiHl2AD0gPVknKsaW0un+3PuM6TTcPMUAWEURKXNso0e5OFPaZYasFtsxNoDemsFOXbvf7SIcnyAkFX/4u37NTfx7g+0IqLXw6QIPolr1PvCSZz8Z5wjBNakeCVozGGOiuCOQDy60XNqfbgrOjxgQ5y/u54K4g7R/xuWmpdx5OMAbUbcy3WbhPCbJJYTI5Hg8C/gsbHSnC2EeOCuyA9ImrNyjsUHkLEh9y4WoRw7lRIc1x+dli8jSJxt9C+NYVUIqK7MEeCmmVyFEGN8mNnqZp4vTe98kxAr4dWSmhcQahHGuFBhKQLlVOdlJ/OT+WPX1zS2UmnkTrxun+FWpCC5bLDlwhlslxtyaN9pV3sRLO6KXM88ZkefRrH21DdR+4j79HA7VLTAsebI79t9nMgmXJ5hB1JKcJMUAgWpxT7C7JUGcWCPIG10NuCd9XQ7H4ykQ4Ve6J2LuNo9SbvP6jPwdfQJB6fJBnKg4mtNuLMlQ4pnXDc+wJmqgw25NfHpFmrZYACZOtLEJoPtMWxxwDzZEYYfT";
-        
-        this.report = new Stimulsoft.Report.StiReport();
-        this.viewer = new Stimulsoft.Viewer.StiViewer(null, 'StiViewer', false);
 
+		this.options = new Stimulsoft.Designer.StiDesignerOptions();
+		this.options.appearance.fullScreenMode = true;
+
+        this.report = new Stimulsoft.Report.StiReport(); 
         this.report.loadFile("/assets/reports/SimpleList.mrt");
+        		
+        this.viewer = new Stimulsoft.Viewer.StiViewer(null, 'StiViewer', false);
         this.viewer.report = this.report;
-        Stimulsoft.Base.StiLicense.Key = "6vJhGtLLLz2GNviWmUTrhSqnOItdDwjBylQzQcAOiHl2AD0gPVknKsaW0un+3PuM6TTcPMUAWEURKXNso0e5OFPaZYasFtsxNoDemsFOXbvf7SIcnyAkFX/4u37NTfx7g+0IqLXw6QIPolr1PvCSZz8Z5wjBNakeCVozGGOiuCOQDy60XNqfbgrOjxgQ5y/u54K4g7R/xuWmpdx5OMAbUbcy3WbhPCbJJYTI5Hg8C/gsbHSnC2EeOCuyA9ImrNyjsUHkLEh9y4WoRw7lRIc1x+dli8jSJxt9C+NYVUIqK7MEeCmmVyFEGN8mNnqZp4vTe98kxAr4dWSmhcQahHGuFBhKQLlVOdlJ/OT+WPX1zS2UmnkTrxun+FWpCC5bLDlwhlslxtyaN9pV3sRLO6KXM88ZkefRrH21DdR+4j79HA7VLTAsebI79t9nMgmXJ5hB1JKcJMUAgWpxT7C7JUGcWCPIG10NuCd9XQ7H4ykQ4Ve6J2LuNo9SbvP6jPwdfQJB6fJBnKg4mtNuLMlQ4pnXDc+wJmqgw25NfHpFmrZYACZOtLEJoPtMWxxwDzZEYYfT";
 
+        // this.dataSet = new Stimulsoft.System.Data.DataSet("SimpleDataSet");
+        // this.dataSet.readJson(data);
+        // this.report.regData(this.dataSet.dataSetName, "", this.dataSet);
+                        
         this.viewer.renderHtml('viewer');
       }
 }
