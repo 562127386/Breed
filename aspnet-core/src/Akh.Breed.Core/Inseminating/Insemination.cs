85224aee0d89e0cdc47e7827daf835bb571cc3b0
Amir <amirkharazian@gmail.com>
2020-06-17 16:20:21 +0430

    add Inseminating data model

---
+++ aspnet-core/src/Akh.Breed.Core/Inseminating/Insemination.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.ComponentModel;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using Abp;
+using Abp.Authorization.Users;
+using Abp.Domain.Entities;
+using Abp.Domain.Entities.Auditing;
+using Abp.Timing;
+using Akh.Breed.BaseInfo;
+using Akh.Breed.Contractors;
+using Akh.Breed.Herds;
+using Akh.Breed.Officers;
+
+namespace Akh.Breed.Inseminating
+{
+    [Table("AkhInseminating")]
+    public class Insemination : Entity, IHasCreationTime, IMayHaveTenant, ICreationAudited
+    {
+        [Required]
+        public string NationalCode { get; set; }
+
+        public DateTime CreationTime { get; set; }
+        
+        public int? TenantId { get; set; }
+        
+        public Insemination()
+        {
+            CreationTime = Clock.Now;
+        }
+
+        public long? CreatorUserId { get; set; }
+    }
+}
